2f50771c35bf23fb00fbe76aad7375fc665db15a
Sophia M Silveira <sophiasilveira2003@gmail.com>
2022-05-26 23:18:45 -0300

    a litle more

--- DocumentExprezz/sair.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    ob_start();
-    unset($_SESSION['CPF'],  $_SESSION['nome'],  $_SESSION['id_I']);
-    
-    $_SESSION['msg'] = "<br> Deslogado com sucesso!"; 
-
-    header("Location:login.php");
-?>
